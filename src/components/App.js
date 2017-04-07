b56efd25a08dc9e0c9d25de742befadf2f79e028
piotr-aueternum <piotr.aueternum@gmail.com>
2017-04-07 12:43:27 +0200

    add css managment via css-modules

---
+++ src/components/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import styles from './App.css';
+
+export default () => <div className={styles.App}>Hello world</div>;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import test from './test';
+import App from './components/App';
 
 window.addEventListener('load', () => {
-  ReactDOM.render(<div>{test()}</div>, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 });
